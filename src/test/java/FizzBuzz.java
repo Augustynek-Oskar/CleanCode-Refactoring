8a68ef0e8ad7d11579b95fbd7d1c572248f430c4
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-21 16:35:37 +0100

    fizzbuzz method

--- src/test/java/FizzBuzz.java
+++ src/test/java/FizzBuzz.java
@@ -1,3 +1,19 @@
 public class FizzBuzz {
+    public static void main(String[] args) {
+        getFizzBuzzGame();
 
+    }
+
+    public static void getFizzBuzzGame() {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+            if (i % 3 == 0) {
+                System.out.println("Fizz" + i);
+            }if (i % 5 == 0) {
+                System.out.println("Buzz" + i);
+            }if ((i % 3 == 0) && (i % 5 == 0)){
+                System.out.println("FizzBuzz" + i);
+            }
+        }
+    }
 }
