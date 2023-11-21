54cc02253316b7d50de31883d9579cab0aacf2e0
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-21 15:05:45 +0100

    Added method that checks if word is blank if not it checks how long it is and if it's a palindrome

--- src/main/java/TextChecker.java
+++ src/main/java/TextChecker.java
@@ -1,5 +1,26 @@
 public class TextChecker {
     public static void main(String[] args) {
+        getTextInfo("oko");
+        getTextInfo("koko");
+    }
 
+    public static void getTextInfo(String text){
+        if (text.isBlank()){
+            System.out.println("There is no text!");
+        }
+
+        String reverse = "";
+        int length = text.length();
+        if (!(text.isBlank())){
+            for (int i = length -1; i >= 0; i--)
+                reverse = reverse + text.charAt(i);
+            if(text.equals(reverse))
+                System.out.println(text + " is a palindrome");
+            else {
+                System.out.println(text + " is not a palindrome");
+            }
+
+            System.out.println("Text length is " + text.length() + " characters \n");
+        }
     }
 }
