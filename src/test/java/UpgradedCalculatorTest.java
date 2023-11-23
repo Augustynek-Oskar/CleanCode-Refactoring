acd4cd032a3b29164e8d6189ab66eb229bd99ce6
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-23 21:55:52 +0100

    Methods tests for each operations

---
+++ src/test/java/UpgradedCalculatorTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UpgradedCalculatorTest {
+
+    @Test
+    void shouldAddOnePlusTwo() {
+        int result = UpgradedCalculator.add(1,2);
+        Assertions.assertEquals(3,result);
+    }
+
+    @Test
+    void shouldSubtractTwoFromFour() {
+        int result = UpgradedCalculator.subtract(4,2);
+        Assertions.assertEquals(2,result);
+    }
+
+    @Test
+    void shouldMultiplyTwoWithThree() {
+        int result = UpgradedCalculator.multiply(2,3);
+        Assertions.assertEquals(6,result);
+    }
+
+    @Test
+    void shouldDivideFourByTwo() {
+        float result = UpgradedCalculator.divide(4,2);
+        Assertions.assertEquals(2,result);
+    }
+}
