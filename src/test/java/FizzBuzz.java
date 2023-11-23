d8b72f09bc6cdfea6a7f82448f673e1cc0c4faac
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-23 21:39:31 +0100

    Added methods for each dividing game

--- src/test/java/FizzBuzz.java
+++ src/test/java/FizzBuzz.java
@@ -1,20 +1,49 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-        getFizzBuzzGame();
+
+        getFizzBuzzIfNumberIsDivisibleByThreeAndFive();
 
     }
 
-    public static void getFizzBuzzGame() {
+    public static void getFizzIfNumberIsDivisibleByThree() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else System.out.println(i);
+        }
+    }
+
+    public static void getBuzzIfNumberIsDivisibleByFive() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else System.out.println(i);
+        }
+    }
+
+    public static void getFizzBuzzIfNumberIsDivisibleByThreeAndFive() {
         for (int i = 1; i <= 100; i++) {
             if ((i % 3 == 0) && (i % 5 == 0)) {
                 System.out.println("FizzBuzz");
             }
-            else if (i % 5 == 0) {
+            else System.out.println(i);
+        }
+    }
+
+    public static void getFizzBuzzGame(){
+        for (int i = 1; i <= 100; i++){
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            }
+            if (i % 5 == 0) {
                 System.out.println("Buzz");
             }
-            else if (i % 3 == 0) {
+            if (i % 3 == 0) {
                 System.out.println("Fizz");
-            }else System.out.println(i);
+            }
+            else System.out.println(i);
         }
     }
 }
