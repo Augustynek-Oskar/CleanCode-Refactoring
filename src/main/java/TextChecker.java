1b7a2773b092df9d2e7161973699b832a38358f5
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-23 21:21:32 +0100

    isTextPalindrome ignores case

--- src/main/java/TextChecker.java
+++ src/main/java/TextChecker.java
@@ -1,7 +1,7 @@
 public class TextChecker {
     public static void main(String[] args) {
 
-        showTextInfo("oko");
+        showTextInfo("Oko");
 
     }
 
@@ -17,7 +17,7 @@ public class TextChecker {
             int length = text.length();
             for (int i = length - 1; i >= 0; i--)
                 reverse = reverse + text.charAt(i);
-            if (text.equals(reverse))
+            if (text.equalsIgnoreCase(reverse))
                 System.out.println(text + " is a palindrome");
             else {
                 System.out.println(text + " is not a palindrome");
