f2d373024fa924fc0fdc9eb6b7be481167b2728d
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-24 21:57:41 +0100

    Methods tests for each operations

--- src/test/java/UpgradedCalculatorTest.java
+++ src/test/java/UpgradedCalculatorTest.java
@@ -1,9 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class UpgradedCalculatorTest {
+public class UpgradedCalculatorTest {
 
     @Test
     void shouldAddOnePlusTwo() {
